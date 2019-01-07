9c6fb535154e814fd75d0cf9fe242f0d71127184
Burak Tilek <burak@Buraks-MacBook-Pro.local>
2019-01-07 04:01:26 +0300

    product_date area added to page layout & calculateDate functionality implemented

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -11,6 +11,24 @@ const formatPrice = (price) => {
 return `$${price/100}`
 }
 
+/**
+ * Function calculates the difference between now and the date item added to the db
+ * 
+ * @param Number date 
+ * @return String or Date object
+ */
+const calculateDate = (date) => {
+    const oneWeekOfMilliSecs = 604800000
+    const dateAdded = new Date(date)
+    const diffMilliSecs = new Date() - dateAdded
+    const days = diffMilliSecs/1000/60/60/24
+    if (diffMilliSecs > oneWeekOfMilliSecs) {
+        return date
+    } else {
+        return `${Math.round(days)} days ago`
+    }
+}
+
 const ProductList = (props) => (
     <ul className="products_container">
         {props.products.map((product, i) => (
@@ -26,8 +44,9 @@ const ProductList = (props) => (
                 {product.face}
                 </div>
                 <div className="product_content">
-                <div><span className="label">Price: </span>{formatPrice(product.price)}</div>
-                <div className="product_size"><span className="label">Size: </span>{product.size}px</div>
+                    <div className="product_price"><span className="label">Price: </span>{formatPrice(product.price)}</div>
+                    <div className="product_size"><span className="label">Size: </span>{product.size}px</div>
+                    <div className="product_date"><span className="label">Date added: </span>{calculateDate(product.date)}</div>
                 </div>
             </li>)
         ))}
