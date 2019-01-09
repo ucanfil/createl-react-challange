3cc3b58d929b964e3798ff0d51dd88fd49b20766
Burak Tilek <burak@Buraks-MacBook-Pro.local>
2019-01-09 04:48:15 +0300

    With every render changing ad bug fixed

--- frontend/src/components/Ads.js
+++ frontend/src/components/Ads.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as ProductsAPI from './ProductsAPI';
 
-/**
- * Function generates random consecutively different numbers
- * 
- * @return Number
- */
-const randomAdGenerator = () => {
-    let previousAdNum
-    do {
-        previousAdNum = Math.floor(Math.random() * 10)
-    } while (previousAdNum === randomAdGenerator.currentAdNum)
-    randomAdGenerator.currentAdNum = previousAdNum
-    return previousAdNum
-}
+class Ads extends Component {
+    state = {
+        adNum: 0,
+    }
 
-/**
- * React stateless component as pure function
- */
-const Ads = () => (
-    <div className="advertisement_container">
-        <img className="ad" src={`${ProductsAPI.API_URL}/ads/?r=${randomAdGenerator()}`} alt="advertisement"/>
-    </div>
-)
+    componentDidMount() {
+        this.randomAdGenerator()
+    }
+
+    /**
+     * Function generates random consecutively different numbers
+     * 
+     * @return Number
+     */
+    randomAdGenerator = () => {
+        let currentAdNum
+        do {
+            currentAdNum = Math.floor(Math.random() * 10)
+        } while (currentAdNum === this.state.adNum)
+        this.setState({ adNum: currentAdNum })
+        return this.state.adNum
+    }
+
+    render() {
+        return (
+            <div className="advertisement_container">
+                <img className="ad" src={`${ProductsAPI.API_URL}/ads/?r=${this.state.adNum}`} alt="advertisement"/>
+            </div>
+        )
+    }
+}
 
 export default Ads;
